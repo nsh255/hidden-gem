cde349d28a2ede2406f65d91312d605ed26df97a
Naoufal <nsh255@inlumine.ual.es>
2025-05-02 20:33:52 +0200

    reset

--- backend/app/scraper/steam_scraper/__init__.py
+++
@@ -1,3 +0,0 @@
-# Este archivo indica que el directorio es un paquete Python
-# Importaciones para facilitar el acceso directo a componentes principales
-from app.scraper.steam_scraper.spiders.games_spider import GamesSpider
